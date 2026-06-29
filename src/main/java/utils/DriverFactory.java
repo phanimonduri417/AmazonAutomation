f2e7c9687c571a06f3471de70d560d4f03f0c459
Phani Monduri <phanimonduri417@gmail.com>
2026-06-29 23:44:57 +0530

    Run Chrome in headless mode for GitHub Actions

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -2,9 +2,18 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
     public static WebDriver createDriver() {
+        ChromeOptions options = new ChromeOptions();
+
+        //Required for GitHub Actions
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         return driver;
